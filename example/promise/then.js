c449270feb94727df47e72b1e58e7793116dd62c
mirreal <zgkm181142521@gmail.com>
2018-04-05 18:16:49 +0800

    add example: then

---
+++ example/promise/then.js
@@ -0,0 +1,15 @@
+import Promise from '../../lib/promise/'
+
+
+// test
+const promise1 = new Promise(function(resolve, reject) {
+    // setTimeout(resolve, 2000, 'foo');
+
+    setTimeout(function() {
+        resolve('数据1')
+    }, 2000)
+})
+
+promise1.then(data => {
+    console.log(data)
+})
